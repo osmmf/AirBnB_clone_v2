2d81f13e176ba1724eb0f9e5f069384132997bb7
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-12-18 13:52:30 +0100

    AirBnB_clone_v2

--- models/review.py
+++ models/review.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 """ holds class Review"""
 import models
 from models.base_model import BaseModel, Base
@@ -11,14 +11,14 @@ class Review(BaseModel, Base):
     """Representation of Review """
     if models.storage_t == 'db':
         __tablename__ = 'reviews'
-        place_id = column(string(60), ForeignKey('places.id'), nullable=False)
-        user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         text = Column(String(1024), nullable=False)
     else:
         place_id = ""
         user_id = ""
         text = ""
-    
+
     def __init__(self, *args, **kwargs):
         """initializes Review"""
         super().__init__(*args, **kwargs)
